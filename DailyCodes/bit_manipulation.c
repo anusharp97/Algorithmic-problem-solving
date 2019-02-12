68195106ee330bd5814b3c9ae6ee4e25fb610298
anusharp97 <35512779+anusharp97@users.noreply.github.com>
2019-02-12 14:29:09 +0530

    Update bit_manipulation.c

--- DailyCodes/bit_manipulation.c
+++ DailyCodes/bit_manipulation.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void bit_manipulation(char data[],int set_size)
+{
+    int i,j;
+    unsigned int size=pow(2,set_size);
+    for(i=0;i<size;i++)
+    {
+        for(j=0;j<set_size;j++)
+        {
+            if(i &(1<<j))
+            {
+                printf("%c",data[j]);
+            }
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    char data[3]={'a','b','c'};
+    int set_size=3;
+    bit_manipulation(data,set_size);
+    return 0;
+}
 
